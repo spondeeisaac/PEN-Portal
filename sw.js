005f8ec27ddc3c28d9e7ead481f2701af98e2847
spondeeisaac <130838721+spondeeisaac@users.noreply.github.com>
2026-09-26 22:10:33 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'PEN-FIELD-VISIT-2026.6';
+const CACHE_NAME = 'PEN-FIELD-VISIT-2026.8';
 const APP_URL = new URL('./', self.location.href).href;
 const SW_URL = new URL('./sw.js', self.location.href).href;
 const MANIFEST_URL = new URL('./manifest.webmanifest', self.location.href).href;
